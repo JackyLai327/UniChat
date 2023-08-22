3172b00d298cfbf971d8d83ca83bda5382cf0a8e
JackyLai327 <jackylai327@gamil.com>
2023-08-22 15:39:12 +1000

    Initial Commit

---
+++ UniChat/UniChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniChatApp.swift
+//  UniChat
+//
+//  Created by Jacky Lai on 2023/8/22.
+//
+
+import SwiftUI
+
+@main
+struct UniChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
